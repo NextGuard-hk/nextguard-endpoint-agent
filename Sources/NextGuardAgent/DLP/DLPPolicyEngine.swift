6ab9c0f746ef98da2cf50d9319210481b8f435ef
NextGuard-hk <oscar@next-guard.com>
2026-03-06 08:49:03 +0800

    chore(version): Bump DLPPolicyEngine.swift to v2.2.0

--- Sources/NextGuardAgent/DLP/DLPPolicyEngine.swift
+++ Sources/NextGuardAgent/DLP/DLPPolicyEngine.swift
@@ -281,7 +281,7 @@ final class AgentConfig {
     var managementServerURL: String = "https://console.next-guard.com"
     var apiToken: String = ""
     var deviceId: String = ""
-    var agentVersion: String = "2.1.0"
+    var agentVersion: String = "2.2.0"
 
     private init() {
         deviceId = getDeviceId()
